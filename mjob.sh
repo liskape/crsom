9e7341b1fd1c369687c170674c723c17bf3932da
pliska <liskape8@gmail.com>
2015-10-26 23:54:27 +0100

    aaas

--- mjob.sh
+++ mjob.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-module add matlab
+#PBS -l matlab=1
+#PBS -l matlab_Neural_Network_Toolbox=1
 
-while 1
-    [tf msg] = license('checkout','Neural_Network_Toolbox');
-    if tf==1, break, end
-    display(strcat(datestr(now),' waiting for licence '));
-    pause(5);
-end
 
 # spust MATLAB davkove
-matlab -nosplash -nodisplay -nodesktop -r "poc(),exit(0)"
+matlab -nosplash -nodisplay -nodesktop -r "metacentrum_run(),exit(0)"  > error.out 2>&1
 
 if [ $? -eq 1 ] ; then
  #MATLAB skoncil s chybou
  tail -n 30 $JOB_NAME.out | mail -s "error in $JOB_NAME" $USER@arien.metacentrum.cz &
 else
-  mail -s "metacentrum matlab job: $JOB_NAME finnished" $USER@arien.metacentrum.cz &
+  echo "finished" | mail -s "metacentrum matlab job: $JOB_NAME finnished" $USER@arien.metacentrum.cz &
 fi
