b5c3bce8a4f030c8679f4f75681abd9699cd6f44
pliska <liskape8@gmail.com>
2015-10-27 00:40:47 +0100

    aassss

--- mjob.sh
+++ mjob.sh
@@ -6,11 +6,7 @@
 cd /storage/brno2/home/$USER/matlab/
 
 module add matlab
-matlab -nosplash -nodisplay -nodesktop -r "poc(),exit(0)"
+matlab -nosplash -nodisplay -nodesktop -r "metacentrum_run(),exit(0)"
 
-if [ $? -eq 1 ] ; then
- #MATLAB skoncil s chybou
- tail -n 30 $JOB_NAME.out | mail -s "error in $JOB_NAME" liskape8@gmail.com &
-else
-  echo "finished" | mail -s "metacentrum matlab job: $JOB_NAME finnished" liskape8@gmail.com &
-fi
+
+
