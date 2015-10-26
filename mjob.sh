ef584c2ffae16938a67523064c810236a45f6d02
pliska <liskape8@gmail.com>
2015-10-26 23:24:44 +0100

    Do not change workspace

--- mjob.sh
+++ mjob.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 module add matlab
 
-# jdi do naseho pracovniho adresare
-cd /storage/praha1/home/liskape8/diplomka/crsom/
-
 # spust MATLAB davkove
 echo "exit(1)"|matlab -nosplash -nodisplay -nodesktop -r "metacentrum_run(),exit(0)" >$JOB_NAME.out 2>&1
 
@@ -13,7 +10,3 @@ if [ $? -eq 1 ] ; then
 else
   mail -s "metacentrum matlab job: $JOB_NAME finnished" $USER@arien.metacentrum.cz &
 fi
-
-git add -A
-git commit -m "$JOB_NAME"
-git push
