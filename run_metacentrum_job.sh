b9a52e403ff9b2f4624efdb7294229d8342d4b5c
pliska <liskape8@gmail.com>
2015-10-29 23:37:26 +0100

    nojvm

---
+++ run_metacentrum_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# push
+ssh liskape8@skirit.ics.muni.cz
+
+
+# pull
+
+
+cp  /storage/brno2/home/$USER/matlab/
+
+qsub mjob.sh
+
+
+git clean -f
+
+
+qsub -m e mjob.sh -d $PWD -q long -l mem=1gb
