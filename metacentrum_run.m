e9bb72309ddb4c8f4c81411ccbc38a1f4a705968
pliska <liskape8@gmail.com>
2015-12-06 22:59:39 +0100

    todo = 'compare with different settings'; epochs = 1000; normalize = true; LR2 = 3; s_0 = 200; s_end = 0.01; settings = 'random_weights_mapstd_normalization'; problem = 'f_train_iris'; net_name = sprintf('%s_epochs=%d_normalize=%i_LR2=%.2f_s_0=%d_s_end=%.2f_%s', problem, epochs, normalize, LR2, s_0, s_end,settings);

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -3,7 +3,7 @@ function metacentrum_run
 todo = 'compare with different settings';
 epochs = 1000;
 normalize = true;
-LR2 = 1.5;
+LR2 = 3;
 s_0 = 200; 
 s_end = 0.01;
 settings = 'random_weights_mapstd_normalization'; 
