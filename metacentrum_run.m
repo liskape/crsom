d36b9ed648f0501b3e020ab5a40adb5c8751cd63
pliska <liskape8@gmail.com>
2016-01-03 21:33:00 +0100

    todo = 'compare with other led_b_*'; epochs = 200; normalize = 2; % 0 = do not normalize, 1 = minmax, 2 = mapstd LR2 = 2; s_0 = 200; s_end = 0.01; settings = 'random_weights'; problem = 'led_b'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -3,7 +3,7 @@ function metacentrum_run
 todo = 'compare with other led_b_*';
 epochs = 200;
 normalize = 2; % 0 = do not normalize, 1 = minmax, 2 = mapstd  
-LR2 = 1;
+LR2 = 2;
 s_0 = 200;
 s_end = 0.01;
 settings = 'random_weights'; 
