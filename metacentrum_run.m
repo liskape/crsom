1aece262d86a597919b22982ff31f980a6cd101f
pliska <liskape8@gmail.com>
2016-01-05 22:36:14 +0100

    todo = 'compare with other katy_soad*'; epochs = 2000; normalize = 3; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.1; s_0 = 200; s_end = 0.01; settings = 'random_weights'; problem = 'katy_soad'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -1,7 +1,7 @@
 function metacentrum_run    
 % *************************************************************************
 todo = 'compare with other katy_soad*';
-epochs = 1000;
+epochs = 2000;
 normalize = 3; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
 LR2 = 0.1;
 s_0 = 200;
