9b68797360db01212e4285bb52923e4154b67c81
pliska <liskape8@gmail.com>
2016-01-06 16:53:33 +0100

    todo = 'should have nic results'; epochs = 1000; normalize = 0; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.05; s_0 = 200; s_end = 0.01; settings = 'som_init'; problem = 'iris'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -2,7 +2,7 @@ function metacentrum_run
 % *************************************************************************
 todo = 'should have nic results';
 epochs = 1000;
-normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
+normalize = 0; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
 LR2 = 0.05;
 s_0 = 200;
 s_end = 0.01;
