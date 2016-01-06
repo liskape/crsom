d846419740413851f57ae4f06ce1118b612f3cb6
pliska <liskape8@gmail.com>
2016-01-06 22:45:25 +0100

    todo = 'smaller LR'; epochs = 2000; normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.05; s_0 = 200; s_end = 0.01; settings = 'som_init'; problem = 'iris'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -1,9 +1,9 @@
 function metacentrum_run    
 % *************************************************************************
-todo = 'bigger LR';
+todo = 'smaller LR';
 epochs = 2000;
 normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
-LR2 = 0.2;
+LR2 = 0.05;
 s_0 = 200;
 s_end = 0.01;
 settings = 'som_init'; 
