abbfbfd8f82121f56778ee08d71faf5a8c54aed2
pliska <liskape8@gmail.com>
2016-01-07 02:46:08 +0100

    todo = 'smaller map'; epochs = 1000; normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.1; s_0 = 200; s_end = 0.01; settings = 'som_init_100'; problem = 'iris'; map_size = 0.75;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -3,10 +3,10 @@ function metacentrum_run
 todo = 'smaller map';
 epochs = 1000;
 normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
-LR2 = 0.05;
+LR2 = 0.1;
 s_0 = 200;
 s_end = 0.01;
-settings = 'som_init'; 
+settings = 'som_init_100'; 
 problem = 'iris';
 map_size = 0.75;
 net_name = sprintf('%s_ep=%d_norm=%i_size=%.2f_LR2=%.2f_%s', problem, epochs, normalize,map_size, LR2,settings);
