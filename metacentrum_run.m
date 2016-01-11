82a120348f5de3894935712f6e911d9694561dc9
pliska <liskape8@gmail.com>
2016-01-12 00:13:20 +0100

    todo = ''; epochs = 1000; normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.1; s_0 = 200; s_end = 0.01; settings = ''; problem = 'hits'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -1,16 +1,16 @@
 function metacentrum_run    
 % *************************************************************************
 todo = '';
-epochs = 2000;
-normalize = 4; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
-LR2 = 0.15;
+epochs = 1000;
+normalize = 1; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5
+LR2 = 0.1;
 s_0 = 200;
 s_end = 0.01;
 settings = ''; 
 problem = 'hits';
 map_size = 1;
 net_name = sprintf('%s_ep=%d_norm=%i_size=%.2f_LR2=%.2f_%s', problem, epochs, normalize,map_size, LR2,settings);
-% net_name = 'dunno'
+% net_name = 'dunno3'
 % -------------------------------------------------------------------------
 
     while 1
