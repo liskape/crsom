ae688cb02e1cf98e812e8271925de7f3f8926f8d
pliska <liskape8@gmail.com>
2016-01-12 22:55:33 +0100

    todo = ''; epochs = 1000; normalize = 4; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4 = minmax 0-0,5 LR2 = 0.2; s_0 = 200; s_end = 0.01; settings = ''; problem = 'off-sexp'; map_size = 1;

--- metacentrum_run.m
+++ metacentrum_run.m
@@ -6,10 +6,10 @@ normalize = 4; % 0 = do not normalize, 1 = minmax, 2 = mapstd, 3 = minmax 0-1, 4
 LR2 = 0.2;
 s_0 = 200;
 s_end = 0.01;
-settings = ''; 
-problem = 'katy_soad_2';
+settings = '';
+problem = 'off-sexp';
 map_size = 1;
-net_name = sprintf('%s_ep=%d_norm=%i_size=%.2f_LR2=%.2f_%s', problem, epochs, normalize,map_size, LR2,settings);
+net_name = sprintf('%s_ep=%d_norm=%i_size=%.2f_LR2=%.2f_%s', problem, epochs, normalize,map_size, LR2, settings);
 % net_name = 'dunno3'
 % -------------------------------------------------------------------------
 
@@ -20,7 +20,7 @@ net_name = sprintf('%s_ep=%d_norm=%i_size=%.2f_LR2=%.2f_%s', problem, epochs, no
         pause(5);
     end 
 
-    [crsom, in, ta, uc ] = train_katy_soad_2(epochs, net_name, normalize, LR2, s_0, s_end, map_size );
+    [crsom, in, ta, uc ] = train_problem(problem, epochs, net_name, normalize, LR2, s_0, s_end, map_size );
     save(strcat(net_name,'.mat'));
     
 %     delete this shit
---
+++ train_problem.m
@@ -0,0 +1,29 @@
+function [ crsom, inputs, targets, untrained, lr2 ] = train_problem(problem, epochs, net_name, normalized, LR2, s0, s_end, map_size)
+  
+    data = importdata(strcat(problem, '-in'))
+    in = data.data;
+    
+    if normalized == 1
+        inputs = mapminmax(in');
+    elseif normalized == 2
+        inputs = mapstd(in');
+    elseif normalized == 0
+        inputs = in
+    elseif normalized == 3
+        inputs = mapminmax(in', 0);
+    elseif normalized == 4
+        inputs = mapminmax(in', 0, 0.5);
+    end
+      
+    targets = importdata(strcat(problem, '-ta'))'
+    crsom = create_crsom(inputs, targets, LR2, s0, s_end, map_size);
+    
+    crsom.trainParam.epochs = epochs;
+    crsom.userdata.net_name = net_name;
+    untrained = crsom;
+    crsom = train(crsom, inputs);
+    
+    % actual data 
+    % gscatter(inputs(1,:), inputs(2,:), vec2ind(targets)')
+end
+
