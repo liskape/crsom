08a6eff436ffa72d6c465730066252fd9d9c643e
pliska <liskape8@gmail.com>
2016-01-14 23:39:11 +0100

    asdas

--- metacentrum_run_batched.m
+++ metacentrum_run_batched.m
@@ -20,6 +20,7 @@ net_name = create_net_name(problem, epochs, normalize, map_size, LR2, settings);
     save(strcat(net_name,'.mat'));
     
     my_plot_som(crsom, in, ta);
+    title(netname);
     print(strcat(net_name,'.png'), '-dpng');
 end
 
