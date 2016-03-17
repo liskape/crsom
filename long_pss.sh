dc87a72a180758723827891be9117073b36d6c36
pliska <liskape8@gmail.com>
2016-03-17 13:31:34 +0100

    PROBLEMS="met_mad_la" LEARNING_RATES="0.1" EPOCHS="500" NORMALIZE="4" MAP_SIZE="1"

---
+++ long_pss.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -l matlab=1
+#PBS -l matlab_Neural_Network_Toolbox=1
+#PBS -l mem=1gb
+#PBS -m e
+#PBS -l nodes=1#excl
+
+PROBLEMS="year_2010vs1990"
+LEARNING_RATES="0.1 0.05"
+EPOCHS="200 500 1000"
+NORMALIZE="3 4 5"
+MAP_SIZE="1 0.5"
+
+for p in `echo $PROBLEMS`
+do
+  for lr in `echo $LEARNING_RATES`
+  do
+    for e in `echo $EPOCHS`
+    do
+      for n in `echo $NORMALIZE`
+      do
+        for ms in `echo $MAP_SIZE`
+        do
+          export p
+          export lr
+          export e
+          export n
+          export ms
+
+          qsub -v p,lr,e,n,ms -N "$p-lr$lr-e$e-n$n-ms$ms"  -q long mjob-one.sh
+       done
+      done
+    done
+  done
+done
+
