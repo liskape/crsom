0d5307e73a85e49c1ccd1c7d8894e4f05bc287b9
pliska <liskape8@gmail.com>
2016-03-24 15:09:25 +0100

    Try this

--- crsom_o.m
+++ crsom_o.m
@@ -1,4 +1,7 @@
 function result = crsom_o( win, indexes, t, distances, t_end, s_0, send )
-    result = exp(power(crsom_s(t, t_end, s_0, send),-1)  * (-distances(indexes, win)));
+    distances =  distances(indexes, win);
+    s = crsom_s(t, t_end, s_0, send);
+    powered_s = power(s,-1);
+    result = exp(powered_s  * (-distances));
 end
 
