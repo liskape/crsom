4f48e3e3cc6b1781919543cf763304e22c94e158
pliska <liskape8@gmail.com>
2016-04-26 16:25:46 +0200

    for p in `echo $PROBLEMS` do   for lr in `echo $LEARNING_RATES`   do

--- create_crsom.m
+++ create_crsom.m
@@ -63,7 +63,7 @@ function [ crsom ] = create_crsom(inputs, targets, LR2, s_0, s_end, map_size)
     
     
     
-    som.userdata = struct('targets', targets, 'enable_snapping', true, 'store_errors', true,'enable_logging', false, 'computing_details', true, 'errors', [], 'net_name', '', 'mean_adjusts', [], 'history_snaps', [], 'context_snaps', [], 'snapped_epochs', [], 'lr2', LR2, 's_0', s_0, 's_end', s_end, 'mean_O_h', [], 'first_part', [],'second_part', [], 'observed_delta_h', [], 'observed_koefs', []);
+    som.userdata = struct('targets', targets, 'enable_snapping', false, 'store_errors', true,'enable_logging', false, 'computing_details', false, 'errors', [], 'net_name', '', 'mean_adjusts', [], 'history_snaps', [], 'context_snaps', [], 'snapped_epochs', [], 'lr2', LR2, 's_0', s_0, 's_end', s_end, 'mean_O_h', [], 'first_part', [],'second_part', [], 'observed_delta_h', [], 'observed_koefs', []);
     som = configure(som, inputs);
 
   cn = context_net2(ones(nodes_size*nodes_size,1), ones(r_tar,1));
--- long_pss.sh
+++ long_pss.sh
@@ -2,7 +2,7 @@
 
 PROBLEMS="hot_or_not_smallest hot_or_not3c_smallest hot_or_not_small hot_or_not3c_small hot_or_not hot_or_not3c"
 LEARNING_RATES="0.05 0.1"
-EPOCHS="2000 5000 7500 10000"
+EPOCHS="1000 2000 5000 7500 10000"
 NORMALIZE="4 5"
 MAP_SIZE="1"
 
