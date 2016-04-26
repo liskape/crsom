432bb75546e4b9a7a5bc30a65ff7e45b8f9460a4
pliska <liskape8@gmail.com>
2016-04-26 16:21:35 +0200

    for p in `echo $PROBLEMS` do   for lr in `echo $LEARNING_RATES`   do

--- long_pss.sh
+++ long_pss.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PROBLEMS="year_2010vs1970_small year_1960vs1970 user_4e11f"
-LEARNING_RATES="0.01 0.05 0.1"
-EPOCHS="2000 5000 10000"
+PROBLEMS="hot_or_not_smallest hot_or_not3c_smallest hot_or_not_small hot_or_not3c_small hot_or_not hot_or_not3c"
+LEARNING_RATES="0.05 0.1"
+EPOCHS="2000 5000 7500 10000"
 NORMALIZE="4 5"
 MAP_SIZE="1"
 
